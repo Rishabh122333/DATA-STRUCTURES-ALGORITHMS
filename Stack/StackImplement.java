ea819f368bec8d0506cb1b4b8218d47df6d09520
Rishabh122333 <102612221+Rishabh122333@users.noreply.github.com>
2022-06-06 23:17:11 +0530

    Implementing Stack data structure using generic class.

---
+++ Stack/StackImplement.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+public class StackImplement {
+    public static void main(String[] args) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
