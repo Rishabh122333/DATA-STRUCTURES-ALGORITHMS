ed7898bfcaacc4e5a79f505d420896835a302b2c
Rishabh122333 <102612221+Rishabh122333@users.noreply.github.com>
2022-06-12 02:24:12 +0530

    Design and Implement Special Stack Data Structure.
    Design a Data Structure SpecialStack that supports all the stack operations like push(), pop(), isEmpty(), isFull() and an additional operation getMin() which should return minimum element from the SpecialStack. All these operations of SpecialStack must be O(1). To implement SpecialStack, you should only use standard Stack data structure and no other data structure
    Example: 
    
    Consider the following SpecialStack
    16  --> TOP
    15
    29
    19
    18
    
    When getMin() is called it should 
    return 15, which is the minimum 
    element in the current stack. 
    
    If we do pop two times on stack, 
    the stack becomes
    29  --> TOP
    19
    18
    
    When getMin() is called, it should 
    return 18 which is the minimum in 
    the current stack.

---
+++ Stack/SpecialStack.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class SpecialStack {
+    Stack<Integer> stack=new Stack<>();
+    Stack<Integer> min=new Stack<>();
+    public void push(int x)
+    {
+        if(stack.isEmpty() || min.isEmpty())
+        {
+            stack.push(x);
+            min.push(x);
+            return;
+        }
+        stack.push(x);
+        if(stack.peek()<min.peek())
+        {
+            min.pop();
+            min.push(x);
+        }
+    }
+    public int pop()
+    {
+        if(stack.isEmpty())
+            return -Integer.MIN_VALUE;
+        int x=stack.pop();
+        if(x==min.peek())
+            min.pop();
+        return x;
+    }
+    public int getMin()
+    {
+        return min.peek();
+    }
+    public static void main(String[] args)
+    {
+        SpecialStack s = new SpecialStack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.getMin());
+        s.push(5);
+        System.out.println(s.getMin());
+    }
+}
