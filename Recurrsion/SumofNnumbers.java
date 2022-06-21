05621570b0758972f71ec5d83a827f9bb4b9109b
Rishabh122333 <102612221+Rishabh122333@users.noreply.github.com>
2022-06-21 22:31:03 +0530

    Calculate Sum of N numbers using recursion.

---
+++ Recurrsion/SumofNnumbers.java
@@ -0,0 +1,12 @@
+public class SumofNnumbers {
+    public static int sum(int n)
+    {
+        if(n==0)
+            return 0;
+        return n+sum(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(6));
+    }
+}
